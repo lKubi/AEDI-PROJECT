6fdcd78822f2814d62eaaee6543bb8e35aca2dbf
Luis <luizfperezm@gmail.com>
2023-04-15 12:52:54 +0200

    Añadido el fichero Mesa.java
    Se añadió el fichera Mesa.java.
    
    Escribí también 2 cabeceras de funciones para que tengan idea de lo que hay que hacer según dice al comienzo de la clase Mesa.
    
    Están comentadas las 3 funciones a implementar

---
+++ Cinquillo_de_oros/src/es/uvigo/esei/aed1/core/Mesa.java
@@ -0,0 +1,40 @@
+/*
+* Representa la Mesa de juego. 
+* Estructura: se utilizar� un TAD adecuado. Piensa que hay 4 palos y de cada palo se pueden colocar cartas 
+* por cualquiera de los dos extremos (un array con 4 doblescolas parece lo más adecuado). La DobleCola se comento en clase de teoria
+* Funcionalidad: saber si es posible colocar una carta en la mesa, colocar una carta en la mesa, mostrar la mesa
+ */
+package es.uvigo.esei.aed1.core;
+
+
+public class Mesa {
+
+	
+    //constructor
+    public Mesa(){
+     
+    }
+
+    //Añadir más funcionalidades
+    
+    //Estas 2 cabeceras de funciones las añadí yo (Luis) para que se implementen, 
+    //saco la idea de las funciones, de la cabecera de la clase
+    
+    
+//    public boolean comprobarSiSePuedenPonerCartas(Jugador j){
+//        
+//    }
+    
+//    public void colocarCartaMesa(Carta c){
+//        
+//    }
+
+    
+    // mostrar el estado de la mesa
+//    @Override
+//    public String toString(){
+//        
+//    }
+	
+    
+}
